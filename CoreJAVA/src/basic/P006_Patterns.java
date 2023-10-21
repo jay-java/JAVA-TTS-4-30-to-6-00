858b8acf2c9e0db8a907f772f4b60037e1244c60
jay-java <jayprajapati.tops@gmail.com>
2023-10-21 17:54:49 +0530

    array loop

---
+++ CoreJAVA/src/basic/P006_Patterns.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class P006_Patterns {
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=i;j++) {
+				if((i+j)%2 == 0) {
+					System.out.print("1");
+				}
+				else {
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
