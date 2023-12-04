262fa022a2c2aa0ee077d72395ae570f48cb8869
jay-java <jayprajapati.tops@gmail.com>
2023-12-04 16:56:59 +0530

    advanceintro

---
+++ AdvanceJAVA/src/main/java/model/User.java
@@ -0,0 +1,49 @@
+package model;
+
+public class User {
+	private int id;
+	private String name,address,email,password;
+	private long contact;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", password="
+				+ password + ", contact=" + contact + "]";
+	}
+	
+}
